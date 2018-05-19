cc7b147b3232a4145967a0b295e956c9b99b6643
Unknown <jans13@web.de>
2018-05-19 22:40:41 +0200

    added code

--- RepaironDemand/app/src/main/java/de/repair/repairondemand/AuftragAnsicht.java
+++ RepaironDemand/app/src/main/java/de/repair/repairondemand/AuftragAnsicht.java
@@ -1,15 +1,26 @@
 package de.repair.repairondemand;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.sql.Blob;
+
+import de.repair.repairondemand.SQLlite.SQLite;
+import de.repair.repairondemand.SQLlite.SQLiteInit;
+
 public class AuftragAnsicht extends AppCompatActivity implements View.OnClickListener {
 
+    private SQLite sqLite;
     private Button mBtnZurück, mBtnProfil, mBtnAngebotAbgeben;
 
     private ImageView mImViAuftragBild;
@@ -25,6 +36,9 @@ public class AuftragAnsicht extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.auftrag_ansicht);
         bindViews();
         init();
+        // über Intent von AngebotSuchen.java, kommt für den
+        // folgenden Aufruf die ID des Auftrags
+        getAuftrag("1");
     }
 
 
@@ -58,6 +72,56 @@ public class AuftragAnsicht extends AppCompatActivity implements View.OnClickLis
                 startActivityIntent = new Intent(this, Profile.class);
                 startActivity(startActivityIntent);
                 break;
+        }
+    }
+
+    public void getAuftrag(String auftragsId){
+        sqLite = new SQLite(this);
+        // Gets the data repository in write mode
+        SQLiteDatabase db = sqLite.getReadableDatabase();
+        String beschreibung;
+        String adresse_fk;
+        byte[] bild;
+        String stadt;
+        try{
+            Log.e("cursor","start");
+            Cursor cursor =
+                    db.query(SQLiteInit.TABLE_ANFRAGE, // a. table
+                            new String[]{SQLiteInit.COLUMN_BESCHREIBUNG, SQLiteInit.COLUMN_ADRESSE_ID_FK,
+                            SQLiteInit.COLUMN_BILD}, // b. column names
+                            " anfrage_id_pk = ? ", // c. selections
+                            new String[] {auftragsId}, // d. selections args
+                            null, // e. group by
+                            null, // f. having
+                            null, // g. order by
+                            null); // h. limit
+            cursor.moveToFirst();
+            beschreibung = cursor.getString(0);
+            adresse_fk = cursor.getString(1);
+            bild = cursor.getBlob(2);
+
+            cursor =
+                    db.query(SQLiteInit.TABLE_ADRESSE, // a. table
+                            new String[]{SQLiteInit.COLUMN_ORT}, // b. column names
+                            " adresse_id_pk = ? ", // c. selections
+                            new String[] {adresse_fk}, // d. selections args
+                            null, // e. group by
+                            null, // f. having
+                            null, // g. order by
+                            null); // h. limit
+            cursor.moveToFirst();
+            stadt = cursor.getString(0);
+            setValues(beschreibung, stadt, bild);
+        }catch(Exception ex){
+        }
+    }
+
+    public void setValues(String beschreibung, String stadt, byte[] bild) {
+        mTvBeschreibung.setText(beschreibung);
+        mTvStandort.setText(stadt);
+        if(bild != null){
+            mImViAuftragBild.setImageBitmap(
+                    BitmapFactory.decodeByteArray(bild, 0, bild.length));
         }
     }
 }
--- RepaironDemand/app/src/main/java/de/repair/repairondemand/AufträgeSuchen.java
+++ RepaironDemand/app/src/main/java/de/repair/repairondemand/AufträgeSuchen.java
@@ -1,12 +1,131 @@
 package de.repair.repairondemand;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+//import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class AufträgeSuchen extends AppCompatActivity implements View.OnClickListener {
 
+    private int checkBtn;
+    public Button mBtnZurück, mBtnRepAnfang, mBtnRepEnde, mBtnSuchen;
+    private Spinner mSpinKategorie;
+    private SeekBar mSeekbar;
+    private TextView mTvRadius;
+    // private ListView mListResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.anfrage_suche);
+        bindViews();
+        init();
+    }
+
+    private void bindViews() {
+        mBtnZurück = this.findViewById(R.id.zurück);
+        mBtnRepAnfang = this.findViewById(R.id.btnDateRepAnfang);
+        mBtnRepEnde = this.findViewById(R.id.btnDateRepEnde);
+        mSpinKategorie = this.findViewById(R.id.kategorie);
+        mBtnSuchen = this.findViewById(R.id.btnSuchen);
+        mTvRadius = this.findViewById(R.id.txtRadius);
+        ArrayAdapter<CharSequence> adapterKategorie = ArrayAdapter.createFromResource(this, R.array.category,
+                android.R.layout.simple_spinner_dropdown_item);
+        mSpinKategorie.setAdapter(adapterKategorie);
+        mSeekbar = this.findViewById(R.id.seekBar);
+        // mListResult = this.findViewById(R.id.listView);
+    }
+
+    private void init() {
+        mBtnZurück.setOnClickListener(this);
+        mBtnRepAnfang.setOnClickListener(this);
+        mBtnRepEnde.setOnClickListener(this);
+        mBtnSuchen.setOnClickListener(this);
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Write code to perform some action when progress is changed.
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Write code to perform some action when touch is stopped.
+                mTvRadius.setText(String.valueOf(seekBar.getProgress()) + " km");
+            }
+        });
+    }
+
     @Override
     public void onClick(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.zurück:
+                finish();
+                break;
+            case R.id.btnDateRepAnfang:
+                checkBtn = 1;
+                showDatePickerDialog();
+                break;
+            case R.id.btnDateRepEnde:
+                checkBtn = 2;
+                showDatePickerDialog();
+                break;
+            case R.id.btnSuchen:
+                // getAufträgeDb();
+                break;
+        }
+    }
 
+    public void showDatePickerDialog() {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(this.getFragmentManager(), "datePicker");
+    }
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            AufträgeSuchen aufträgeSuchen = (AufträgeSuchen) getActivity();
+            aufträgeSuchen.date(year, month, day);
+        }
+    }
+
+    public void date(int year, int month, int day){
+        if(checkBtn==1) {
+            mBtnRepAnfang.setText(String.valueOf(day) + "." + String.valueOf(month) + "." +
+                    String.valueOf(year));
+        }else if(checkBtn==2) {
+            mBtnRepEnde.setText(String.valueOf(day) + "." + String.valueOf(month) + "." +
+                    String.valueOf(year));
+        }
     }
 }
