80be34f604262ca72737d442b4d5c22aa7127d79
Unknown <onurkaraman1993@outlook.de>
2018-06-08 17:12:42 +0200

    added code

--- RepaironDemand/app/src/main/java/de/repair/repairondemand/Beschwerde.java
+++ RepaironDemand/app/src/main/java/de/repair/repairondemand/Beschwerde.java
@@ -1,4 +1,104 @@
 package de.repair.repairondemand;
 
-public class Beschwerde {
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public class Beschwerde extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnZurück, mBtnSenden, mBtnDatum;
+    private Intent startActivityIntent;
+    private String username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beschwerde);
+        username = getIntent().getExtras().getString("username");
+        bindViews();
+        init();
+    }
+
+    private void bindViews() {
+        mBtnZurück = this.findViewById(R.id.btnZurück);
+        mBtnSenden = this.findViewById(R.id.btnSenden);
+        mBtnDatum = this.findViewById(R.id.btnDate);
+    }
+
+    private void init() {
+        mBtnDatum.setOnClickListener(this);
+        mBtnSenden.setOnClickListener(this);
+        mBtnZurück.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.btnZurück:
+                startActivityIntent =  new Intent(this, ServiceCenter.class);
+                startActivityIntent.putExtra("username", username);
+                startActivity(startActivityIntent);
+                break;
+            case R.id.btnSenden:
+                Toast.makeText(this, "Abgeschickt.", Toast.LENGTH_LONG).show();
+                startActivityIntent =  new Intent(this, Home.class);
+                startActivityIntent.putExtra("username", username);
+                startActivity(startActivityIntent);
+                break;
+            case R.id.btnDate:
+                showDatePickerDialog();
+                break;
+        }
+    }
+
+    public void showDatePickerDialog() {
+        DialogFragment newFragment = new AnfrageErstellen.DatePickerFragment();
+        newFragment.show(this.getFragmentManager(), "datePicker");
+    }
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            Beschwerde beschwerde = (Beschwerde) getActivity();
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year, month, day);
+
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            String strDate = format.format(calendar.getTime());
+
+            beschwerde.date(strDate);
+        }
+    }
+
+    public void date(String date){
+        mBtnDatum.setText(date);
+    }
 }
