eb9bc761533802bde0c935bb3db1c808ef411a21
Unknown <zink_selina@web.de>
2018-06-21 15:58:31 +0200

    Log out

--- RepaironDemand/app/src/main/java/de/repair/repairondemand/MainActivity.java
+++ RepaironDemand/app/src/main/java/de/repair/repairondemand/MainActivity.java
@@ -37,7 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Drawable house, bagger;
 
-
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
